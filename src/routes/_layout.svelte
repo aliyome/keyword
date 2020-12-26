61e13eb4f58017102685617722a993281881563f
aliyome <aliyome@gmail.com>
2020-12-27 01:45:11 +0900

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Nav from '../components/Nav.svelte';
+
+  export let segment: string;
+</script>
+
+<style global lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+</style>
+
+<Nav segment="{segment}" />
+
+<main class="grid mx-auto max-w-screen-xl p-6 text-gray-800">
+  <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-6xl mx-0 mt-0 mb-4 p-0 text-gray-800 break-words">Great success!</h1>
+
+<figure>
+  <img alt="Success Kid" src="{successkid}" />
+  <figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p>
+  <strong>Try editing this file (src/routes/index.svelte) to test live
+    reloading.</strong>
+</p>
