451afa3c1a93c287028ff8be87ea56e5c9dea02b
aliyome <aliyome@gmail.com>
2020-12-27 16:14:40 +0900

    gomi

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,31 @@
 import * as sapper from '@sapper/app';
 
+import firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/performance';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBx2-DCDiTEPogaljDXM1xro9JNj-HwD-0',
+  authDomain: 'suggest-keyword.firebaseapp.com',
+  projectId: 'suggest-keyword',
+  storageBucket: 'suggest-keyword.appspot.com',
+  messagingSenderId: '58584803187',
+  appId: '1:58584803187:web:470a505c7ce0feaa368706',
+  measurementId: 'G-1G3QSJTB6X',
+};
+// Initialize Firebase
+const initFirebase = () => {
+  const app = firebase.initializeApp(firebaseConfig);
+  const anal = firebase.analytics();
+  const perf = firebase.performance();
+  const auth = firebase.auth();
+  return { app, anal, perf, auth };
+};
+
 sapper.start({
   target: document.querySelector('#sapper'),
 });
+
+// @ts-ignore
+window.firebase = initFirebase();
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
+  // client-only code here
+
+  //  import { FirebaseApp, User, Doc, Collection } from 'sveltefire';
+
+  import firebase from 'firebase/app';
+  import 'firebase/analytics';
+  import 'firebase/performance';
+  import 'firebase/auth';
+
   import Nav from '../components/Nav.svelte';
   import Footer from '../components/Footer.svelte';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBx2-DCDiTEPogaljDXM1xro9JNj-HwD-0',
+      authDomain: 'suggest-keyword.firebaseapp.com',
+      projectId: 'suggest-keyword',
+      storageBucket: 'suggest-keyword.appspot.com',
+      messagingSenderId: '58584803187',
+      appId: '1:58584803187:web:470a505c7ce0feaa368706',
+      measurementId: 'G-1G3QSJTB6X',
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  });
 </script>
 
 <style global lang="postcss">
